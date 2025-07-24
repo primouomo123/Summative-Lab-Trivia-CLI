fa56bf8350d7a73cda1f80befde11b3997cc0e49
primouomo123 <luismanuelrios123@gmail.com>
2025-07-24 11:32:51 -0400

    I've added comments and the time

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Summative-Lab-Trivia-CLI
+
+Inside the Summative-Lab-Trivia-CLI folder you can eaither:
+1. run **node bin/index.js** to run the game
+
+2. Make the CLI executable and link it globally by running:
+$ chmod +x bin/index.js
+$ npm link
+AFter the 2 previous commands were run, you can just run **Summative-Lab-Trivia-CLI** to run the trivia game.
--- src/lib/gameLogic.js
+++ src/lib/gameLogic.js
@@ -2,107 +2,138 @@ import chalk from "chalk";
 import { select } from "@inquirer/prompts";
 import gameState from "./state.js";
 
+//This is the function to show the Main Menu of the game
 export async function showMainMenu() {
-    const action = await select({
-        message: "Welcome to the Trivia CLI\nYou have 20 seconds to finish the game.\n \nMain Menu",
+  const action = await select({
+    //I wrote a message informing the user that they have 5 seconds to answer each question
+    message: "Welcome to the Trivia CLI\nYou have 5 seconds to finish each question.\n \nMain Menu",
     choices: [
       { name: "Start Game", value: "start" },
       { name: "See Stats", value: "stats" },
       { name: "Reset Stats", value: "reset" },
       { name: "Quit", value: "quit" },
     ],
-    });
+  });
 
-    switch (action) {
-        case "start":
-            await startGame();
-            break;
-        case "stats":
-            showStats();
-            await select({ message: "Press Enter to go back", choices: [{ name: "Back", value: "back" }] });
-            showMainMenu();
-            break;
-        case "reset":
-            resetGame();
-            console.log(chalk.blue("Stats have been reset."));
-            showMainMenu();
-            break;
-        case "quit":
-            console.log("Goodbye!");
-            process.exit(0);
+  //With the switch, a different function is called depending on the action the user selected
+  switch (action) {
+    case "start":
+      await startGame();
+      break;
+    case "stats":
+      showStats();
+      await select({ message: "Press Enter to go back", choices: [{ name: "Back", value: "back" }] });
+      await showMainMenu();
+      break;
+    case "reset":
+      resetGame();
+      console.log(chalk.blue("Stats have been reset."));
+      await showMainMenu();
+      break;
+    case "quit":
+      console.log("Goodbye!");
+      process.exit(0);
   }
 }
 
+//This is the function that performs the game
 export async function startGame() {
+  //This is the array of questions for the game. Each element is an object.
   const questions = [
-        {question: "What is the capital of the United States?",
-            choices: ["Ottawa", "Washington State", "Washington DC", "Kingston"],
-            answer: "Washington DC"
-        },
+    {
+      question: "What is the capital of the United States?",
+      choices: ["Ottawa", "Washington State", "Washington DC", "Kingston"],
+      answer: "Washington DC",
+    },
+    {
+      question: "What is the capital of Brazil?",
+      choices: ["Rio de Janeiro", "Sao Paulo", "Bahia", "Brasilia"],
+      answer: "Brasilia",
+    },
+    {
+      question: "What is the capital of Morocco?",
+      choices: ["Casablanca", "Rabat", "Madrid", "Granada"],
+      answer: "Rabat",
+    },
+    {
+      question: "What is the capital of Thailand?",
+      choices: ["Bangkok", "Beijing", "Tokyo", "Laos"],
+      answer: "Bangkok",
+    },
+    {
+      question: "What is the capital of Kenya?",
+      choices: ["Angola", "Cairo", "Congo", "Nairobi"],
+      answer: "Nairobi",
+    },
+  ];
 
-        {question: "What is the capital of Brazil?",
-            choices: ["Rio de Janeiro", "Sao Paulo", "Bahia", "Brasilia"],
-            answer: "Brasilia"
-        },
+  //This is the loop to iterate over each question
+  for (const question of questions) {
+    try { /* The code of what's supposed to happen is in the catch */
+      const answer = await Promise.race([ /* With promise.race I can have an array of 2 promises,
+        so it lets me wait for the user to answer or jump to the next question if
+        no answer within 5 secodns */
+        select({ /* Select is the first posible promise */
+          message: question.question,
+          choices: question.choices.map(choice => ({
+            name: choice,
+            value: choice,
+          })),
+        }),
 
-        {question: "What is the capital of Morocco?",
-            choices: ["Casablanca", "Rabat", "Madrid", "Granada"],
-            answer: "Rabat"
-        },
+        //This new promise is what's going to happen if no answer within 5 seconds
+        new Promise((_, reject) =>
+             /* If no answer within 5 seconds, this promise will throw an error to be managed
+        by catch */
+          setTimeout(() => reject(new Error("timeout")), 5000)
+        ),
+      ]);
 
-        {question: "What is the capital of Thailand?",
-            choices: ["Bangkok", "Beijing", "Tokyo", "Laos"],
-            answer: "Bangkok"
-        },
-
-        {question: "What is the capital of Kenya?",
-            choices: ["Angola", "Cairo", "TokyCongo", "Nairobi"],
-            answer: "Nairobi"
-        },
-    ];
-
-    const totalQuestions = questions.length;
-    let answeredQuestions = 0;
-    let unansweredQuestions = 0;
-
-  for(const question of questions) {
-        const answer = await select({
-            message: question.question,
-            choices: question.choices.map(choice => {
-                return {name: choice, value: choice}
-            })
-        });
-        answeredQuestions++;
-        unansweredQuestions = totalQuestions - answeredQuestions
-        determineIfCorrect(answer, question, unansweredQuestions);
-
-    }
-
-    showStats()
-    showMainMenu();
-}
-
-export function determineIfCorrect(answer, question, unansweredQuestions) {
-    if(answer === question.answer) {
+      // If answered before timeout:
+      if (answer === question.answer) { //This happens if the answer is correct
         gameState.correct++;
-        console.log(chalk.green("Correct!"))
-    } else {
+        console.log(chalk.green("Correct!"));
+      } else { //This happens if the answer is incorrect
         gameState.incorrect++;
-        console.log(chalk.red("Incorrect!"))
+        console.log(chalk.red("Incorrect!"));
+      }
     }
 
-    gameState.unanswered = unansweredQuestions;
+    //Catch manages the error if time's up
+    catch (err) {
+      if (err.message === "timeout") {
+        console.log(chalk.yellow("Time's up! Moving to the next question."));
+        gameState.unanswered++;
+      } else {
+        console.error("Unexpected error:", err);
+      }
+    }
+  }
+
+  //I show the current game stat after each game ends
+  showStats();
+
+  //showMainMenu is called after each game is finished
+  await showMainMenu();
 }
 
 export function showStats() {
-    console.log(chalk.blue("Game Statistics:"));
-    console.log(chalk.green(`Correct answers: ${gameState.correct}`));
-    console.log(chalk.red(`Incorrect answers: ${gameState.incorrect}`));
-    console.log(chalk.yellow(`Unanswered questions: ${gameState.unanswered}`));
+  
+  //If there's no stats, this will be the message to be shown when this function is called  
+  if (gameState.correct === 0 && gameState.incorrect === 0 && gameState.unanswered === 0) {
+    console.log(chalk.yellow("You don't have any game stats, yet"));
+    return;
+  }
+
+  //When there's stats, these are the messages to be shown
+  console.log(chalk.blue("Game Statistics:"));
+  console.log(chalk.green(`Correct answers: ${gameState.correct}`));
+  console.log(chalk.red(`Incorrect answers: ${gameState.incorrect}`));
+  console.log(chalk.yellow(`Unanswered questions: ${gameState.unanswered}`));
 }
 
 export function resetGame() {
-    gameState.correct = 0;
-    gameState.incorrect = 0;
-    gameState.unanswered = 0;
+  gameState.correct = 0;
+  gameState.incorrect = 0;
+  gameState.unanswered = 0;
 }
